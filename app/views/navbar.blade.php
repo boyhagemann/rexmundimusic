c0a623ec2a4bf74af62cf37bc4083252cc59c4fa
= <=>
2014-04-17 16:41:50 +0200

    update html css js

--- app/views/navbar.blade.php
+++ app/views/navbar.blade.php
@@ -1,14 +1,20 @@
 
     <nav id="navbar" class="navbar navbar-inverse navbar-main navbar-fixed-top" role="navigation">
-        <div class="container">
+        <div class="container-fluid">
           <!-- Brand and toggle get grouped for better mobile display -->
-          <div class="navbar-header logo">              
+          <div class="navbar-header logo">
+			  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+				  <span class="sr-only">Toggle navigation</span>
+				  <span class="icon-bar"></span>
+				  <span class="icon-bar"></span>
+				  <span class="icon-bar"></span>
+			  </button>
             <a class="navbar-brand logo__link" href="#">Rex Mundi Music</a>
           </div>
 
           <!-- Collect the nav links, forms, and other content for toggling -->
-          <div class="collapse navbar-collapse navbar-main__menu">
-            <ul class="nav navbar-nav pull-right navbar-main__menu-list">
+          <div class="collapse navbar-collapse navbar-main__menu" id="navbar-collapse">
+            <ul class="nav navbar-nav navbar-main__menu-list">
               <li class="navbar-main__menu-item"><a href="#section-visual" class="navbar-main__menu-link i__font_main">Latest</a></li>
               <li class="navbar-main__menu-item"><a href="#section-bio" class="navbar-main__menu-link i__font_main">Bio</a></li>
               <li class="navbar-main__menu-item"><a href="#section-listen" class="navbar-main__menu-link i__font_main">Listen</a></li>
